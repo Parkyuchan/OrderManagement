aa80b123213ddc5e79833d511db72be52bb44175
Parkyuchan <dbcks4848@naver.com>
2024-12-07 23:08:35 +0900

    주문 관리 API

---
+++ src/main/java/kr/co/ordermanagement/presentation/dto/ChangeStateRequestDto.java
@@ -0,0 +1,9 @@
+package kr.co.ordermanagement.presentation.dto;
+
+import kr.co.ordermanagement.domain.order.State;
+
+public class ChangeStateRequestDto {
+    private State state;
+
+    public State getState() { return state; }
+}
